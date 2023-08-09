12c846206d8dceb4e7c1c0c01cdf922c6ba3f0de
Mateus Ceci <mateusceci@gmail.com>
2023-08-08 21:15:38 -0300

    estilizando-sideBar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../Components/Avatar'
+import Paragrafo from '../../Components/Paragrafo'
 import Titulo from '../../Components/Titulo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://avatars.githubusercontent.com/u/135540422?s=400&v=4" />
-    <Titulo fontSize={20}>Mateus Ceci</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Mateus Ceci</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        mateusceci-coder
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
